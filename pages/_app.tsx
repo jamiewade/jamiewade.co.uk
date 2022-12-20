e01ac1f5483d8ff99229a4ed179557b405ade1dd
Jamie Wade <hello@jamiewade.co.uk>
2022-12-20 19:48:49 +0000

    GTM integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import TagManager, { TagManagerArgs } from "react-gtm-module";
+import { useEffect } from "react";
+
 export default function App({ Component, pageProps }: AppProps) {
+	const gtmId = "GTM-WH2XLCN";
+
+	const tagManagerArgs: TagManagerArgs = {
+		gtmId,
+	};
+
+	useEffect(() => {
+		TagManager.initialize(tagManagerArgs);
+	}, []);
+
 	return <Component {...pageProps} />;
 }
